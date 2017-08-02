3b8e498b1fb9614178d1803abe4359b81f68d128
10-1-27-221-WV\ZasypinNV <ZasypinNV@yanos.slavneft.ru>
2017-08-02 13:07:24 +0300

    Array attributes and operations on arrays

--- lesson4/NumPyArrays.py
+++ lesson4/NumPyArrays.py
@@ -28,8 +28,33 @@ def test_run():
     print(np.random.randint(0, 10, size = 5)) # 5 random integers as a 1D array
     print(np.random.randint(0, 10, size=(2, 3))) # 2x3 arra y of random integers
     
-    
+    # Array attributes
+    a = np.random.random((5, 4))
+    print(a.shape)      # number of dimensions
+    print(len(a.shape))
+    print(a.shape[0]) # number of rows
+    print(a.shape[1]) # number of columns
+    print(a.size)     # number of elements in a
+    print(a.dtype)    # dtype of elements
 
+    # Operations on arrays
+    np.random.seed(693) # seed the random number generator
+    a = np.random.randint(0, 10, size=(5, 4)) # 5x4 random integers in [0, 10)
+    print("Array:\n", a)
+    print("Sum of all elements:", a.sum())
+    # axis = 0 - columns, 1 - rows
+
+    #Iterate over rows, to compute sum of each column
+    print("Sum of each column:\n", a.sum(axis=0))
+    #Iterate over columns, to compute sum of each row
+    print("Sum of each row:\n", a.sum(axis=1))
+
+    # Statistics: min, max, mean(across rows, cols, and overall)
+    print("Min of each column:\n", a.min(axis=0))
+    print("Max of each row:\n", a.max(axis=1))
+    print("Mean of all elements:\n", a.mean())
+
+    
     """ functions
 numpy.empty
 numpy.ones
